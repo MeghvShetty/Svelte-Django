219494be84f04700e649d0174bd396622b0e4415
Megh Shetty <meghvshetty@outlook.com>
2023-09-30 11:35:38 +0100

    added bun adapter

--- svelte-spa/svelte.config.js
+++ svelte-spa/svelte.config.js
@@ -1,4 +1,5 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import adapter from "svelte-adapter-bun";
 
 export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
