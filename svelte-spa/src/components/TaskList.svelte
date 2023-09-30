723760a7d74023439dc8c41a259db9b56cbe568e
Megh Shetty <meghvshetty@outlook.com>
2023-09-30 11:11:40 +0100

    create a componet as task add django end point

---
+++ svelte-spa/src/components/TaskList.svelte
@@ -0,0 +1,28 @@
+<!-- src/components/TaskList.svelte -->
+
+<script>
+    let tasks = [];
+  
+    async function fetchTasks() {
+      try {
+        const response = await fetch('http://127.0.0.1:8000/api/tasks'); // Replace with your API URL
+        const data = await response.json();
+        tasks = data;
+      } catch (error) {
+        console.error('Error fetching tasks:', error);
+      }
+    }
+  
+    import { onMount } from 'svelte';
+  
+    onMount(fetchTasks);
+  </script>
+  
+  <h1>Task List</h1>
+  
+  <ul>
+    {#each tasks as task (task.id)}
+      <li>{task.title}</li>
+    {/each}
+  </ul>
+  
